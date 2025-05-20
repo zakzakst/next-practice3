766f9c293533e6ebd513d75ce5c70e63a08d2e29
zakzakst <e1433104@gmai.com>
2025-05-21 07:02:32 +0900

    修正

--- src/app/progress/page.tsx
+++ src/app/progress/page.tsx
@@ -1,11 +1,29 @@
 "use client";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import { Progress } from "@/components/ui/progress";
 
 const Page = () => {
   return (
     <div>
       <Progress value={0} />
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        autoplay
+        modules={[Autoplay, Pagination, Navigation]}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
     </div>
   );
 };
