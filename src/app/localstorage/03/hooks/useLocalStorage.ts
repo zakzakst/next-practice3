249e48373ff239400b0bd65905a182d669b2aa58
zakzakst <e1433104@gmai.com>
2025-06-19 09:35:23 +0900

    修正

---
+++ src/app/localstorage/03/hooks/useLocalStorage.ts
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState<T>(initialValue);
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    try {
+      const item = window.localStorage.getItem(key);
+      if (item !== null) {
+        setStoredValue(JSON.parse(item));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, [key]);
+
+  const setValue = (value: T) => {
+    try {
+      setStoredValue(value);
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(key, JSON.stringify(value));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const removeValue = () => {
+    try {
+      setStoredValue(initialValue);
+      if (typeof window !== "undefined") {
+        window.localStorage.removeItem(key);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue, removeValue] as const;
+};
